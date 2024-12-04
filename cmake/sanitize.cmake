10890b1952371c46946cca854ff750910b6f5b90
peteGSX <peteracole@outlook.com.au>
2024-12-05 09:36:31 +1000

    Initial test framework in place

---
+++ cmake/sanitize.cmake
@@ -0,0 +1,21 @@
+# Courtesy of Vincent Hamp via the DCCEXProtocol library tests
+macro(sanitize SANITIZERS)
+  # Set in current scope
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${SANITIZERS}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=${SANITIZERS}")
+  set(LDFLAGS "${LDFLAGS} -fsanitize=${SANITIZERS}")
+
+  # Set in PARENT_SCOPE
+  get_directory_property(HAS_PARENT_SCOPE PARENT_DIRECTORY)
+  if(HAS_PARENT_SCOPE)
+    set(CMAKE_C_FLAGS
+        ${CMAKE_C_FLAGS}
+        PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS
+        ${CMAKE_CXX_FLAGS}
+        PARENT_SCOPE)
+    set(LDFLAGS
+        ${LDFLAGS}
+        PARENT_SCOPE)
+  endif()
+endmacro()
